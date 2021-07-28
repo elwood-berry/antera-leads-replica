27df00fd3390365af1d5148051a11852cbdabbea
elwood-berry <eberry@anterasoftware.com>
2021-07-28 14:10:11 -0500

    Updated copyrights

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 /*
-APPLICATION NAME GOES HERE
-Author: 'me@elwoodberry.com'
-This code is brought to you by Elwood Berry @www.elwoodberry.com 
+ANTERA: LEADS - REPLICA
+Author: 'eberry@anterasoftware.com'
+This code is brought to you by Elwood Berry @ anterasoftware.com  
 */
 
 import { Component } from '@angular/core';
@@ -15,7 +15,7 @@ export class AppComponent {}
 
 /*
 -------------------------------------------------
-Copyright 2021 ElwoodBerry.com LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that can be found in the LICENSE file at http://www.elwoodberry.com 
+Copyright 2021 anterasoftware.com LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that can be found in the LICENSE file at http://www.anterasoftware.com 
 
 */
